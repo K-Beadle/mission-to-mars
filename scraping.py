53b112a142f9100a8ad65eb31a14a294edbb6b5f
Kenneth Beadle <kbeadle27@gmail.com>
2021-05-17 01:18:51 -0500

    updating progress: adding index.html file

--- scraping.py
+++ scraping.py
@@ -3,26 +3,44 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 
-# set up executable path
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+
+
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary 
+    data = { 
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
+
 
 # ### Article NASA mars news site
-
 def mars_news(browser):
-
 
     # Visit the mars NASA news site
     url = 'https://redplanetscience.com/'
     browser.visit(url)
+
     # Optional delay for laoding the page
     browser.is_element_present_by_css('div.list_text', wait_time=1)
 
     # convert the browser html to a soup object and then quit the browser
     html = browser.html
     news_soup = soup(html, 'html.parser')
-
     
     # Add try/except for error handling
     try:
@@ -43,7 +61,6 @@ def mars_news(browser):
 
 
 # ### Featured Images
-
 def featured_image(browser):
 
     # Visit URL
@@ -90,10 +107,6 @@ def mars_facts():
     return df.to_html()
 
 
-# quit the scraping session
-browser.quit()
-
-
-
-
-
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
